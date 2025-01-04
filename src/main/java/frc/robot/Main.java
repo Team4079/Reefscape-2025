9a422d57e324609b22b994b64742e30d314656fd
Om Gupta <ogupta4242@gmail.com>
2025-01-04 08:16:42 -0800

    Initial commit

---
+++ src/main/java/frc/robot/Main.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public final class Main {
+  /**
+   * Good luck if you change any of this!
+   *
+   * <p>Hello!
+   *
+   * <p>Help I'm stuck in the basement.
+   */
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
+  }
+}
