c0ad29492a9d00ba917f87d6bbc1e9df27d0d838
Erick Tran <thelittlebotengineer@gmail.com>
2025-01-10 13:50:19 -0800

    revert(cz): revert back to cjs from js

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+// import fs from 'fs';
+import path from 'path';
 
 // Path to your .cz-config.js file
 const czConfigPath = path.resolve(__dirname, '.cz-config.js');
