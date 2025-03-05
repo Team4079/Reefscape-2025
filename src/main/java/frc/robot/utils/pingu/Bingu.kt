9c00e52c874f2bb7227d59a89b6173a263c14c05
Om Gupta <ogupta4242@gmail.com>
2025-03-05 10:15:37 -0800

    docs(utils): bingu to reflect new implementation
    BindPingu's not a thing anymore

--- src/main/java/frc/robot/utils/pingu/Bingu.kt
+++ src/main/java/frc/robot/utils/pingu/Bingu.kt
@@ -3,14 +3,13 @@ package frc.robot.utils.pingu
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import frc.robot.commands.Kommand.cmd
 import frc.robot.utils.emu.Button
 
 /**
- * BindPingu is a utility object for binding Xbox controller buttons to commands.
+ * Bingu is a utility object for binding Xbox controller buttons to commands.
  */
 object Bingu : SubsystemBase() {
-    // List to store the mappings of controllers, buttons, and command suppliers
+    /** List to store the mappings of controllers, buttons, and command suppliers */
     private val buttonMaps: MutableList<Triple<XboxController, Button, () -> Command>> = mutableListOf()
 
     /**
@@ -38,19 +37,6 @@ object Bingu : SubsystemBase() {
         button: Button,
         commandSupplier: () -> Command,
     ) = button to commandSupplier
-
-    /**
-     * Creates a pair of a Button and a command supplier function using a runnable.
-     *
-     * @param button The button to bind.
-     * @param runnable A function to be executed when the button is pressed.
-     * @return A pair of the button and the created command supplier.
-     */
-    @JvmStatic
-    fun bind(
-        button: Button,
-        runnable: Runnable,
-    ) = button to cmd { runnable.run() }
 
     /**
      * Periodically checks the state of each button and schedules the corresponding command if the button is pressed.
